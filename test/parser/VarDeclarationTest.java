428df92fe6a835011d45618b8a9584d79ceb06ed
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-04-07 11:22:18 +0100

    Added VarDeclaration tests

---
+++ test/parser/VarDeclarationTest.java
@@ -0,0 +1,37 @@
+package parser;
+
+import org.junit.Test;
+
+public class VarDeclarationTest extends ParserTest {
+    private static final String VAR_DECLARATION_RULE = "VarDeclarations";
+
+    @Test
+    public void singleVarDeclaration() {
+        noErrors("int var;");
+    }
+
+    @Test
+    public void multipleVarDeclarations() {
+        noErrors("int[] a; boolean _b; String $c2; MyType d;");
+    }
+
+    @Test
+    public void missingSemicolon() {
+        mustFail("int var");
+    }
+
+    @Test
+    public void missingVariableName() {
+        mustFail("int;");
+    }
+
+    @Test
+    public void missingType() {
+        mustFail("var;");
+    }
+
+    @Override
+    protected String getStartRule() {
+        return VAR_DECLARATION_RULE;
+    }
+}
