a18c3b30fe86d57b8d2c35e065e47223277c8817
monkin77 <monkinsmurf@gmail.com>
2022-04-12 17:49:55 +0100

    Started working on some visitors (until varDecl)

--- src/pt/up/fe/comp/VisitorEval.java
+++ src/pt/up/fe/comp/VisitorEval.java
@@ -1,5 +1,6 @@
 package pt.up.fe.comp;
 
+import org.eclipse.jgit.diff.ContentSource;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 
@@ -7,35 +8,62 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VisitorEval extends AJmmVisitor<Object, Integer> {
-    private final Map<String, Integer> map;
+    private final Map<String, Pair<String, String>> map;
 
     public VisitorEval() {
-        this.map = new HashMap<String, Integer>();
+        this.map = new HashMap<String, Pair<String, String>>();
 
         /* addVisit("IntegerLiteral", this::integerVisit);
         addVisit("UnaryOp", this::unaryOpVisit);
         addVisit("BinOp", this::binOpVisit);
         addVisit("SymbolReference", this::symbolReference);
         addVisit("Assign", this::assignVisit); */
-        addVisit("_Identifier", this::IdentifierVisit);
-        addVisit("DotMethod", this::DotMethodVisit);
+        addVisit("_Identifier", this::identifierVisit);
+        addVisit("DotMethod", this::dotMethodVisit);
+        addVisit("VarDecl", this::varDeclVisit);
+        addVisit("IntArray", this::intArrayVisit);
 
         setDefaultVisit(this::defaultVisit);
     }
 
-    private Integer IdentifierVisit(JmmNode node, Object dummy) {
+    private Integer identifierVisit(JmmNode node, Object dummy) {
         if (node.getNumChildren() == 0) {
-            System.out.println("Analysing the identifier " + node.get("id"));
+            System.out.println("Analysing the " + node.getKind() + " " + node.get("id"));
             return 0;
         }
 
         throw new RuntimeException("Illegal number of children in node " + node.getKind() + ".");
     }
 
-    private Integer DotMethodVisit(JmmNode node, Object dummy) {
-        if (node.getNumChildren() == 0) {
-            System.out.println("Analysing the DotMethod " + node.get("method"));
+    private Integer dotMethodVisit(JmmNode node, Object dummy) {
+        if (node.getNumChildren() == 0) {   // Could have children?
+            // Check if method exists?
+            System.out.println("Analysing the " + node.getKind() + " " + node.get("id"));
             return 0;
+        }
+
+        throw new RuntimeException("Illegal number of children in node " + node.getKind() + ".");
+    }
+
+    private Integer varDeclVisit(JmmNode node, Object dummy) {
+        if (node.getNumChildren() == 1) {
+            String varName = node.get("name");
+            System.out.println("Analysing the " + node.getKind() + " " + varName);
+
+            String varType = visit(node.getJmmChild(0)) == 1 ? "Int" : "String";    // Create an enum mapping the integer values to each type?
+            Pair<String, String> pair = new Pair(varType, null);
+            this.map.put(varName, pair);    // Store new variable in the map
+
+            return 0;
+        }
+
+        throw new RuntimeException("Illegal number of children in node " + node.getKind() + ".");
+    }
+
+    private Integer intArrayVisit(JmmNode node, Object dummy) {
+        if (node.getNumChildren() == 0) {
+            System.out.println("Analysing the " + node.getKind());
+            return 2;   // Return 2 if IntArray; Return 1 if Int, and so on... ?
         }
 
         throw new RuntimeException("Illegal number of children in node " + node.getKind() + ".");
@@ -60,9 +88,9 @@ public class VisitorEval extends AJmmVisitor<Object, Integer> {
 
     private Integer symbolReference(JmmNode node, Object dummy) {
         if (node.getNumChildren() == 0) {
-            Integer value = this.map.get(String.valueOf(node.get("image")));
-            if (value == null) throw new RuntimeException("Error. Tried to dereference variable" + node.get("image") + "before assignment!");
-            return value;
+            // Integer value = this.map.get(String.valueOf(node.get("image")));
+            //if (value == null) throw new RuntimeException("Error. Tried to dereference variable" + node.get("image") + "before assignment!");
+            // return value;
         }
 
         throw new RuntimeException("Illegal number of children in node " + node.getKind() + ".");
@@ -120,7 +148,7 @@ public class VisitorEval extends AJmmVisitor<Object, Integer> {
                 case EQ:
                     String key = String.valueOf(node.getJmmChild(0).get("image"));
                     Integer value = visit(node.getJmmChild(1));
-                    this.map.put(key, value);
+                    // this.map.put(key, value);
                     return value;
 
                 default:
