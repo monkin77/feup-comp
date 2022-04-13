fb9559f4479145afcb62d3972bc53868bd4b35e4
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-04-13 01:48:46 +0100

    Added instruction labels

--- src/pt/up/fe/comp/jasmin/MethodsBuilder.java
+++ src/pt/up/fe/comp/jasmin/MethodsBuilder.java
@@ -3,6 +3,7 @@ package pt.up.fe.comp.jasmin;
 import org.specs.comp.ollir.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static pt.up.fe.comp.jasmin.JasminConstants.TAB;
 
@@ -37,78 +38,101 @@ public class MethodsBuilder extends AbstractBuilder {
         return builder.toString();
     }
 
-    private void compileMethodBody(Method method) {
+    private void compileMethodBody(final Method method) {
         final ArrayList<Instruction> instructions = method.getInstructions();
         for (final Instruction instruction : instructions) {
             builder.append(TAB);
+            compileInstructionLabels(method, instruction);
+
+            // TODO Maybe create package with instruction builders
             switch (instruction.getInstType()) {
                 case ASSIGN:
+                    // TODO
                     break;
                 case CALL:
-                    final CallInstruction callInstruction = (CallInstruction) instruction;
-                    switch (callInstruction.getInvocationType()) {
-                        case invokevirtual:
-                            builder.append("invokevirtual ");
-                            break;
-                        case invokespecial:
-                            builder.append("invokespecial ");
-                            break;
-                        case invokestatic:
-                            builder.append("invokestatic ");
-                            break;
-                        case invokeinterface:
-                            builder.append("invokeinterface ");
-                            break;
-                        // TODO: What to do with this?
-                        case NEW:
-                            builder.append("new ");
-                            break;
-                        case arraylength:
-                            builder.append("arraylength ");
-                            break;
-                        case ldc:
-                            builder.append("ldc ");
-                            break;
-                    }
-
-                    if (callInstruction.getFirstArg() instanceof Operand) {
-                        builder.append((((Operand) callInstruction.getFirstArg()).getName()));
-                    } else {
-                        // TODO: Remaining cases
-                        builder.append(JasminUtils.getTypeName(callInstruction.getFirstArg().getType(), classUnit));
-                    }
-
-                    if (callInstruction.getSecondArg().isLiteral()) {
-                        final LiteralElement literalElement = (LiteralElement) callInstruction.getSecondArg();
-                        builder.append("/").append(literalElement.getLiteral()).append(" ");
-                    } else {
-                        // TODO: Remaining cases
-//                        builder.append("/").append(callInstruction.getSecondArg().getName()).append(" ");
-                    }
-
-                    for (final Element element : callInstruction.getListOfOperands()) {
-
-                    }
-
+                    compileCallInstruction(instruction);
                     break;
                 case GOTO:
+                    // TODO
                     break;
                 case BRANCH:
+                    // TODO
                     break;
                 case RETURN:
+                    // TODO
                     break;
                 case PUTFIELD:
+                    // TODO
                     break;
                 case GETFIELD:
+                    // TODO
                     break;
                 case UNARYOPER:
+                    // TODO
                     break;
                 case BINARYOPER:
+                    // TODO
                     break;
                 case NOPER:
+                    // TODO
                     break;
+                default:
+                    // TODO Check which cases fit in default
             }
             builder.append("\n");
+        }
+    }
+
+    private void compileInstructionLabels(final Method method, final Instruction instruction) {
+        final List<String> labels = method.getLabels(instruction);
+        for (String label : labels)
+            builder.append(label).append(":\n").append(TAB);
+    }
+
+    private void compileCallInstruction(Instruction instruction) {
+        final CallInstruction callInstruction = (CallInstruction) instruction;
+        switch (callInstruction.getInvocationType()) {
+            case invokevirtual:
+                builder.append("invokevirtual ");
+                break;
+            case invokespecial:
+                builder.append("invokespecial ");
+                break;
+            case invokestatic:
+                builder.append("invokestatic ");
+                break;
+            case invokeinterface:
+                builder.append("invokeinterface ");
+                break;
+            // TODO: What to do with this?
+            case NEW:
+                builder.append("new ");
+                break;
+            case arraylength:
+                builder.append("arraylength ");
+                break;
+            case ldc:
+                builder.append("ldc ");
+                break;
+        }
+
+        if (callInstruction.getFirstArg() instanceof Operand) {
+            builder.append((((Operand) callInstruction.getFirstArg()).getName()));
+        } else {
+            // TODO: Remaining cases
+            builder.append(JasminUtils.getTypeName(callInstruction.getFirstArg().getType(), classUnit));
+        }
+
+        if (callInstruction.getSecondArg().isLiteral()) {
+            final LiteralElement literalElement = (LiteralElement) callInstruction.getSecondArg();
+            builder.append("/").append(literalElement.getLiteral()).append(" ");
+        } else {
+            // TODO: Remaining cases
+//                        builder.append("/").append(callInstruction.getSecondArg().getName()).append(" ");
+        }
+
+        for (final Element element : callInstruction.getListOfOperands()) {
+
         }
     }
 }
