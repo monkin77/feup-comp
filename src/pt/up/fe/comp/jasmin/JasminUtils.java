99866d9da2c22fb0f9dd2ed537ae3e9c178d2ffb
Tomás Morgado Vicente <up201904609@edu.fe.up.pt>
2022-05-16 14:15:26 +0100

    Hotfix for jasmin boolean literals (not considered literals for some reason).

--- src/pt/up/fe/comp/jasmin/JasminUtils.java
+++ src/pt/up/fe/comp/jasmin/JasminUtils.java
@@ -62,6 +62,10 @@ public class JasminUtils {
             // TODO Use iconst, etc. when possible
             builder.append("ldc ").append(elementName).append("\n");
             return builder.toString();
+        } else if (element.getType().getTypeOfElement() == ElementType.BOOLEAN && (elementName.equals("true") || elementName.equals("false"))) {
+            if (elementName.equals("false")) builder.append("iconst_0");
+            else builder.append("iconst_1");
+            return builder.toString();
         }
 
         final Descriptor descriptor = method.getVarTable().get(elementName);
